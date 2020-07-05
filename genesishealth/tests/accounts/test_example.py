6bd27188c489427d3adcace2e846386f565cacca
Gregg Keithley <gregg@pronym.com>
2020-07-05 17:16:27 -0400

    Update PR

--- genesishealth/tests/accounts/test_example.py
+++ genesishealth/tests/accounts/test_example.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 
+from genesishealth.apps.gdrives.models import GDrive
+
 
 class TestExampleTestCase(TestCase):
     def test_math(self):
-        self.assertEqual(1 + 1, 2)
+        device = GDrive.objects.create(
+            meid="123456789",
+            device_id="987654321"
+        )
+        self.assertEqual(device.meid, "1234567890")
