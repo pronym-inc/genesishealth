28381e75261886f60325989d19da2f642c21d177
Gregg Keithley <gregg@pronym.com>
2020-08-09 18:39:51 -0400

    Add option for order category to allow no products.

--- genesishealth/apps/orders/models/order_category.py
+++ genesishealth/apps/orders/models/order_category.py
@@ -4,6 +4,7 @@ from django.db import models
 class OrderCategory(models.Model):
     name = models.CharField(max_length=255, unique=True)
     is_refill = models.BooleanField(default=False)
+    allow_empty_products = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.name
